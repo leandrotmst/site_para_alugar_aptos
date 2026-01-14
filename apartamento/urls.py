74bd04acbf7e99f2fa684dd7a1f9b311c0758f5b
leandro <leandrotomasetto@gmail.com>
2026-01-14 10:01:52 -0300

    PROJETO COMPLETO

--- apartamento/urls.py
+++ apartamento/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "apartamento"
+
 urlpatterns = [
     path("", views.ListaApartamentos.as_view(), name="lista"),
-    path("<slug>", views.DetalheApartamento.as_view(), name="detalhe"),
+    path("<int:pk>/", views.DetalheApartamento.as_view(), name="detalhe"),
     path("busca/", views.Busca.as_view(), name="busca"),
 ]
